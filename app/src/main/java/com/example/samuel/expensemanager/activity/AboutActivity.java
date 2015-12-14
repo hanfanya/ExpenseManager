00cbc00aa16f456443f32cf6beda60ad097f1247
Samuel <xuzhou40@gmail.com>
2015-12-14 23:20:18 +0800

    增加关于本软件界面

--- app/src/main/java/com/example/samuel/expensemanager/activity/AboutActivity.java
+++ app/src/main/java/com/example/samuel/expensemanager/activity/AboutActivity.java
@@ -1,18 +1,101 @@
 package com.example.samuel.expensemanager.activity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.samuel.expensemanager.R;
 import com.example.samuel.expensemanager.utils.SysUtils;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar_about)
+    Toolbar mToolbarAbout;
+    @Bind(R.id.rl_email)
+    RelativeLayout mRlEmail;
+    @Bind(R.id.collapse_toolbar)
+    CollapsingToolbarLayout mCollapseToolbar;
+    @Bind(R.id.tv_version_code)
+    TextView mTvVersionCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(SysUtils.getThemeResId(AboutActivity.this));
-
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+
+        mToolbarAbout.setTitle("关于本软件");
+        setSupportActionBar(mToolbarAbout);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        mCollapseToolbar.setTitleEnabled(false);//固定toolbar标题
+
+
+        mRlEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, "xuzhou40@gmail.com");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "寸金App意见反馈");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(AboutActivity.this, "手机上没有邮件客户端，请使用其他方式联系", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mTvVersionCode.setText(getVersionName());
+
+    }
+
+    private int getVersionCode() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);// 获取包的信息
+
+            int versionCode = packageInfo.versionCode;
+            return versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // 没有找到包名的时候会走此异常
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    private String getVersionName() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);// 获取包的信息
+
+            int versionCode = packageInfo.versionCode;
+            String versionName = packageInfo.versionName;
+
+            System.out.println("versionName=" + versionName + ";versionCode="
+                    + versionCode);
+
+            return versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
